48262c6708ef91142b5804b1b147fc60fdeeeca1
JohnnyFangFangFang <johnny781222@gmail.com>
2023-02-17 16:11:16 +0800

    Create the project

---
+++ app.js
@@ -0,0 +1,30 @@
+// include packages and define server related variables
+const express = require("express");
+const exphbs = require("express-handlebars");
+// 載入自己寫好的另一支 js 檔
+const generatePassword = require("./generate_password");
+const app = express();
+const port = 3000;
+
+// setting template engine
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// setting body-parser
+app.use(express.urlencoded({ extended: true }));
+
+// setting routes
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.post("/", (req, res) => {
+  const options = req.body;
+  const password = generatePassword(options);
+  res.render("index", { password, options });
+});
+
+// starts the express server and listening for connections
+app.listen(port, () => {
+  console.log(`Express app listening on port ${port}.`);
+});
